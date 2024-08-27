e81225cf18c74ade2777fa05a6d25b0b83a4c2ac
Vincent Biret <vincentbiret@hotmail.com>
2024-08-27 10:54:34 -0400

    feat: result blocking benchmark
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

---
+++ src/benchmark/CallingResult/CallingResultBenchamrk.cs
@@ -0,0 +1,39 @@
+using benchmark.Mocks;
+using BenchmarkDotNet.Attributes;
+
+namespace benchmark.CallingResult;
+[MemoryDiagnoser]
+[ThreadingDiagnoser]
+[DisassemblyDiagnoser]
+public class CallingResultBenchmark : IDisposable
+{
+	private readonly HttpClient _httpClient = FunTranslationClient.GetHttpClient();
+	private readonly CancellationTokenSource _cancellationTokenSource = new();
+
+	[Benchmark]
+	public void CallingResult()
+	{
+		var result = _httpClient.GetAsync("https://localhost").Result;
+	}
+	[Benchmark]
+	public async Task CallingAsync()
+	{
+		var result = await _httpClient.GetAsync("https://localhost");
+	}
+	[Benchmark]
+	public async Task CallingWithCancellationTokenAsync()
+	{
+		var result = await _httpClient.GetAsync("https://localhost", _cancellationTokenSource.Token);
+	}
+
+    public void Dispose()
+    {
+        Dispose(true);
+		GC.SuppressFinalize(this);
+    }
+	protected virtual void Dispose(bool disposing)
+    {
+        _httpClient.Dispose();
+		_cancellationTokenSource.Dispose();
+    }
+}
---
+++ src/benchmark/Mocks/FunTranslationClient.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using System.Net.Http.Headers;
+using Moq;
+using Moq.Protected;
+
+namespace benchmark.Mocks;
+
+public static class FunTranslationClient
+{
+	private const string returnJson = 
+		"""
+		{
+			"success": {
+				"total": 1
+			},
+			"contents": {
+				"translated": "Lost a planet,  master obiwan has.",
+				"text": "Master Obiwan has lost a planet.",
+				"translation": "yoda"
+			}
+		}
+		""";
+	public static HttpClient GetHttpClient()
+	{
+		var responseMessage = new HttpResponseMessage
+		{
+			StatusCode = HttpStatusCode.OK,
+			Content = new StringContent(returnJson, MediaTypeHeaderValue.Parse("application/json"))
+		};
+		var mockMessageHandler = new Mock<HttpMessageHandler>();
+		mockMessageHandler.Protected()
+			.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+			.ReturnsAsync(responseMessage);
+
+		// mockMessageHandler.Protected()
+		// 	.Setup<HttpResponseMessage>("Send", It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>())
+		// 	.Returns(responseMessage);
+		
+		return new HttpClient(mockMessageHandler.Object);
+	}
+}
