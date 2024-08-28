1f6c76e4c17eec25c03fc08c3e995b07d68c3532
Vincent Biret <vincentbiret@hotmail.com>
2024-08-28 08:47:21 -0400

    feat: scaffolds analyzer

---
+++ tests/TerribleApiClient.Analyzers.Test/JsonStringDeserializationUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using TestHelper;
+using Xunit;
+using TerribleApiClient.Analyzers;
+
+namespace TerribleApiClient.Analyzers.Test
+{
+    public class JsonStringDeserializationUnitTests : ConventionCodeFixVerifier
+    {
+        [Fact]
+        public void EmptySource() => VerifyCSharpByConvention();
+
+        [Fact]
+        public void LowercaseLetters() => VerifyCSharpByConvention();
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider() => new JsonStringDeserializationCodeFixProvider();
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new JsonStringDeserializationAnalyzer();
+    }
+}
