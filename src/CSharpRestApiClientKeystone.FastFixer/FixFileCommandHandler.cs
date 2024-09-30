f16cf277b5605bb34dbc0397de15e8e99883a88a
Vincent Biret <vincentbiret@hotmail.com>
2024-09-30 16:29:31 -0400

    feat: adds dynamic analyzers loading
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

---
+++ src/CSharpRestApiClientKeystone.FastFixer/FixFileCommandHandler.cs
@@ -0,0 +1,163 @@
+using System.Collections.Immutable;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO.Compression;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+
+namespace CSharpRestApiClientKeystone.FastFixer;
+
+public class FixFileCommandHandler : ICommandHandler
+{
+    public required Argument<List<string>> FilePathsArgument { get; init; }
+    public required Option<List<string>> NugetPackagesOption { get; init; }
+    public required Option<List<string>> DeffectsToFixOption { get; init; }
+    public int Invoke(InvocationContext context)
+    {
+        throw new InvalidOperationException("Use InvokeAsync instead");
+    }
+
+    public async Task<int> InvokeAsync(InvocationContext context)
+    {
+        var filePaths = context.ParseResult.GetValueForArgument(FilePathsArgument).Distinct(StringComparer.OrdinalIgnoreCase).ToList();
+        var nugetPackages = (context.ParseResult.GetValueForOption(NugetPackagesOption) ?? []).Distinct(StringComparer.OrdinalIgnoreCase).ToList();
+        var deffectsToFix = (context.ParseResult.GetValueForOption(DeffectsToFixOption) ?? []).Distinct(StringComparer.OrdinalIgnoreCase).ToHashSet(StringComparer.OrdinalIgnoreCase);
+        var cancellationToken = context.BindingContext.GetService(typeof(CancellationToken)) is CancellationToken token ? token : CancellationToken.None;
+
+        var missingFiles = filePaths.Where(x => !Path.Exists(x)).ToList();
+        if (missingFiles.Count > 0)
+        {
+            Console.WriteLine($"ERROR: Files {string.Join(',', filePaths)} not found");
+            return 1;
+        }
+
+        var loadedAnalyzers = await LoadNugetPackagesAsync(nugetPackages, cancellationToken);
+        var analyzersToApply = loadedAnalyzers.Where(x => deffectsToFix.Contains(x.Key)).ToDictionary(x => x.Key, x => x.Value, StringComparer.OrdinalIgnoreCase);
+
+        var originalSources = new Dictionary<string, string>(filePaths.Count);
+        foreach (var file in filePaths)
+        {
+            originalSources.Add(file, await File.ReadAllTextAsync(file, cancellationToken));
+        }
+
+        throw new NotImplementedException();
+    }
+    private static readonly HttpClient HttpClient = new();
+
+    private async Task<Dictionary<string, DiagnosticAnalyzer>> LoadNugetPackagesAsync(List<string> nugetPackages, CancellationToken cancellationToken)
+    {
+        var result = new Dictionary<string, DiagnosticAnalyzer>();
+        Directory.CreateDirectory("packages");
+        foreach (var package in nugetPackages)
+        {
+            var packageTargetPath = Path.Combine("packages", package.Replace(':', Path.DirectorySeparatorChar));
+            if (!Directory.Exists(packageTargetPath))
+            {
+                using var nuPkg = await HttpClient.GetStreamAsync($"https://www.nuget.org/api/v2/package/{package.Replace(':', '/')}", cancellationToken);
+                using var zip = new ZipArchive(nuPkg);
+                zip.ExtractToDirectory(packageTargetPath, true);
+            }
+            foreach (var dll in Directory.GetFiles(packageTargetPath, "*.dll", SearchOption.AllDirectories))
+            {
+                var assembly = Assembly.LoadFrom(dll);
+                var analyzers = assembly.GetTypes().Where(t => typeof(DiagnosticAnalyzer).IsAssignableFrom(t) && !t.IsAbstract).ToArray();
+                foreach (var analyzerClass in analyzers)
+                {
+                    var analyzer = (DiagnosticAnalyzer)Activator.CreateInstance(analyzerClass);
+                    foreach (var diagnostic in analyzer.SupportedDiagnostics)
+                    {
+                        result.TryAdd(diagnostic.Id, analyzer);
+                    }
+                }
+
+            }
+        }
+        return result;
+    }
+
+    // private async Task FixCSharpAsync(Dictionary<string, string> sources, DiagnosticResult[] expectedResults, params FixResult[] fixResults)
+    // {
+    //     var analyzer = new VSTHRD111UseConfigureAwaitAnalyzer();
+    //     var fix = GetCSharpCodeFixProvider();
+
+    //     var originalProject = CreateProject(sources);
+
+    //     var diagnostics = GetDiagnostics(originalProject, analyzer);
+
+    //     foreach (var fixResult in fixResults)
+    //     {
+    //         var project = await ApplyFixAsync(originalProject, analyzer, fix, fixResult.Index);
+
+    //         var expectedSources = fixResult.ExpectedSources;
+
+    //         if (expectedSources == null || expectedSources.Count == 0)
+    //             return;
+
+    //         var actualSources = new Dictionary<string, string>();
+
+    //         foreach (var doc in project.Documents)
+    //         {
+    //             var code = GetStringFromDocument(doc);
+    //             actualSources.Add(doc.Name, code);
+    //         }
+
+    //         foreach (var item in actualSources)
+    //         {
+    //             var actual = item.Value;
+    //             var newSource = expectedSources[item.Key];
+    //             Assert.Equal(newSource, actual);
+    //         }
+    //     }
+    // }
+    #region ImportedFromUnitTests
+    protected static CSharpCompilationOptions CompilationOptions => new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+    private static readonly DirectoryInfo _coreDir = Directory.GetParent(System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory());
+    protected static readonly IEnumerable<MetadataReference> References =
+    [
+        MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+        MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
+        MetadataReference.CreateFromFile(typeof(CSharpCompilation).Assembly.Location),
+        MetadataReference.CreateFromFile(typeof(Compilation).Assembly.Location),
+        MetadataReference.CreateFromFile(typeof(System.Net.Http.HttpContent).Assembly.Location),
+        MetadataReference.CreateFromFile(typeof(Object).GetTypeInfo().Assembly.Location),
+        MetadataReference.CreateFromFile(_coreDir.FullName + Path.DirectorySeparatorChar + "System.Runtime.dll")
+    ];
+    private const string TestProjectName = "TestProject";
+    private static Project CreateProject(Dictionary<string, string> sources)
+    {
+        var projectId = ProjectId.CreateNewId(debugName: TestProjectName);
+
+        var solution = new AdhocWorkspace()
+            .CurrentSolution
+            .AddProject(projectId, TestProjectName, TestProjectName, LanguageNames.CSharp);
+
+        foreach (var reference in References)
+        {
+            solution = solution.AddMetadataReference(projectId, reference);
+        }
+
+        int count = 0;
+        foreach (var source in sources)
+        {
+            var newFileName = source.Key;
+            var documentId = DocumentId.CreateNewId(projectId, debugName: newFileName);
+            solution = solution.AddDocument(documentId, newFileName, SourceText.From(source.Value));
+            count++;
+        }
+
+        var project = solution.GetProject(projectId)
+            .WithCompilationOptions(CompilationOptions);
+        return project;
+    }
+    private static Diagnostic[] GetDiagnostics(Project project, DiagnosticAnalyzer analyzer)
+    {
+        var compilationWithAnalyzers = project.GetCompilationAsync().Result.WithAnalyzers(ImmutableArray.Create(analyzer));
+        var diagnostics = compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync().Result;
+        return diagnostics.OrderBy(d => d.Location.SourceSpan.Start).ToArray();
+    }
+    #endregion
+}
--- src/CSharpRestApiClientKeystone.FastFixer/Program.cs
+++ src/CSharpRestApiClientKeystone.FastFixer/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+namespace CSharpRestApiClientKeystone.FastFixer;
+public static class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var fileArgument = new Argument<List<string>>("filePath", "The file path to the source file to fix") {
+            Arity = ArgumentArity.OneOrMore,
+        };
+
+        var nugetPackagesOption = new Option<List<string>>("--nuget-packages", "The nuget packages to use for the fixer") {
+            Arity = ArgumentArity.ZeroOrMore,
+        };
+        nugetPackagesOption.SetDefaultValue(new List<string> { "Microsoft.VisualStudio.Threading.Analyzers:17.11.20" });
+
+        var deffectsToFixOption = new Option<List<string>>("--deffects-to-fix", "The deffects to fix") {
+            Arity = ArgumentArity.ZeroOrMore,
+        };
+        deffectsToFixOption.SetDefaultValue(new List<string> { "VSTHRD111" });
+
+        var rootCommand = new RootCommand
+        {
+            fileArgument,
+            nugetPackagesOption,
+            deffectsToFixOption,
+        };
+
+        var fileCommandHandler = new FixFileCommandHandler
+        {
+            FilePathsArgument = fileArgument,
+            NugetPackagesOption = nugetPackagesOption,
+            DeffectsToFixOption = deffectsToFixOption,
+        };
+        rootCommand.Handler = fileCommandHandler;
+        return await rootCommand.InvokeAsync(args);
+    }
+}
+
